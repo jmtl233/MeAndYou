5667a40cca1dcb002ce1752f55f400c1477654a9
jmlt <1739558851@qq.com>
2025-01-19 21:59:56 +0800

    服务器端

---
+++ diary/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+class Diary(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    diary = models.ForeignKey('Diary', on_delete=models.CASCADE, related_name='comments')
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'评论：{self.content[:20]}'
