e8a3c7e52e8374e30545084b843f39af60b5a7a0
jmlt <1739558851@qq.com>
2025-01-22 14:55:08 +0800

    更新于 2025-01-22 14:55:08

---
+++ scripts/update_repo.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+cd /www/wwwroot/www.jmlt.fun || exit
+
+# 保存当前修改
+git stash
+
+# 切换到指定分支
+git checkout 服务器端
+
+# 恢复保存的修改
+git stash pop
+
+# 添加所有更改
+git add -A
+
+# 提交并动态生成提交信息
+git commit -m "更新于 $(date '+%Y-%m-%d %H:%M:%S')"
+
+# 强制推送到远程分支
+git push origin 同步 --force
+
